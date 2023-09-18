8386c14845259fbfe97c73dde97851314887ad1c
wangrui <hary654321@126.com>
2023-09-18 20:04:06 +0800

    res

---
+++ restart.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+git pull
+go build -o scanengin worker.go
+ps -ef | grep ./scanengin | grep -v grep | awk '{print $2}' | xargs kill -9
+cd /zrtx/cy/scanengin && ./scanengin >> scan.log &
