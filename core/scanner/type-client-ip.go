1ca83fa55db185140a85cd0b2b7c4d4f6f185157
wangrui <hary654321@126.com>
2023-09-19 11:05:54 +0800

    8s

--- core/scanner/type-client-ip.go
+++ core/scanner/type-client-ip.go
@@ -28,7 +28,7 @@ func NewIPScanner(config *Config) *IPClient {
 	client.pool.Interval = config.Interval
 	client.pool.Function = func(in interface{}) {
 		ip := in.(net.IP)
-		if ping.Pinger(ip.String(), 1) == nil {
+		if ping.Pinger(ip.String(), 8) == nil {
 			client.HandlerAlive(ip)
 			return
 		}
